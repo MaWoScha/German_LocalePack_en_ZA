bd1db6ed15e6f7bcf7452fefd9ba9a083e46c8e7
MaWoScha <manfredwormschaefer@gmail.com>
2019-08-17 13:54:09 +0200

    Version 0.2.2, small fix in 'Instructions.php'

--- app/code/community/German/LocalePackZa/Block/System/Config/Form/Fieldset/Instructions.php
+++ app/code/community/German/LocalePackZa/Block/System/Config/Form/Fieldset/Instructions.php
@@ -55,7 +55,7 @@ class German_LocalePackZa_Block_System_Config_Form_Fieldset_Instructions
 		$html .= $helper->__("In order to ensure the translation Magento coverage, please see the information in the lower paragraphs!");
         $html .= "</p>";
         $html .= "<p style='text-align:right;'>";
-		$html .= $helper->__("powered by") <a href='http://blog.siempro.co/' target='_blank'>MaWoScha</a>";
+		$html .= $helper->__("powered by")." <a href='https://github.com/mawoscha/German_LocaleFallback' target='_blank'>MaWoScha</a>";
         $html .= "</p>";
         $html .= $this->_getFooterHtml($element);
 
